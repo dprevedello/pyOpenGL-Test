86c1179d1fba8be810632a9a541137bfb36101fa
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-11 19:25:42 +0100

    Interpolazione lineare
    Inviamo l'informazione colore per ciascun vertice. La pipeline utilizzerà l'informazione per colorare la superficie interpolandone i valori.

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ def disegna():
 	glClear(GL_COLOR_BUFFER_BIT)
 	
 	glBegin(GL_TRIANGLES)
-	glColor3f( 1, 0.8, 0 )
+	glColor3f( 1, 0, 0 )
 	glVertex3f( -1, -1, 0 )
-	glVertex3f(  0,  1, 0 )
-	glVertex3f(  1, -1, 0 )
+	glColor3f( 0, 1, 0 )
+	glVertex3f( 0, 1, 0 )
+	glColor3f( 0, 0, 1 )
+	glVertex3f( 1, -1, 0 )
 	glEnd()
 	
 	pygame.display.flip()
