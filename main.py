c378efc74be2f8b3486ad75997d663d8976f12e9
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-11 14:29:02 +0100

    Creiamo il nostro primo canvas
    Creiamo tramite pygame una nuova finestra all'interno della quale potremo disegnare in 3D.
    Colleghiamo agli eventi del sistema alcune semplici azioni.

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+import sys
+import traceback
+
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def disegna():
+	glClearColor(1,0,0,1); # R=1 G=0 B=0 A=1
+	glClear(GL_COLOR_BUFFER_BIT)
+	pygame.display.flip()
+
 
 def main():
-	print("Hello world!")
+	pygame.init()
+	display = (500, 500)
+	screen = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+			if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+				return
+			if event.type == pygame.KEYUP and event.unicode == 'q':
+				return
+
+		disegna()
 
 
 if __name__ == "__main__":
-    main()
+	try:
+		main()
+	except:
+		traceback.print_exc()
+	finally:
+		pygame.quit()
+		sys.exit()
