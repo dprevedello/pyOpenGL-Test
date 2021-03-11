4e86776b577825f9ffd4cbc3cd3ced80a53220b9
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-11 19:22:17 +0100

    Il primo triangolo
    Facciamo disegnare il primo triangolo sul canvas

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ from OpenGL.GLU import *
 
 
 def disegna():
-	glClearColor(1,0,0,1); # R=1 G=0 B=0 A=1
+	glClearColor(0,0,0,1); # R=0 G=0 B=0 A=1
 	glClear(GL_COLOR_BUFFER_BIT)
+	
+	glBegin(GL_TRIANGLES)
+	glColor3f( 1, 0.8, 0 )
+	glVertex3f( -1, -1, 0 )
+	glVertex3f(  0,  1, 0 )
+	glVertex3f(  1, -1, 0 )
+	glEnd()
+	
 	pygame.display.flip()
 
 
