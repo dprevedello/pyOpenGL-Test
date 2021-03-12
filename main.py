1071c548dc2f9a6623a73892a575718a6e4837e5
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-12 01:46:15 +0100

    Aggiungiamo lo zoom
    Colleghiamo al movimento della rotella del mouse e ai tasti + e - una trasformazione di traslazione sull'asse z, così da simulare l'effetto zoom.
    Separiamo inoltre la trasformazione prospettica dalla trasformazione di model-view

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-def disegna():
-	glClearColor(0,0,0,1);
-	glClear(GL_COLOR_BUFFER_BIT)
-	
+def triangolo():
 	glPushMatrix()
-	
+
 	glRotatef(10, 0, 0, 1)
 	glScalef(0.7, 0.5, 1)
 	glTranslatef(0, 0, -1.0)
 	glTranslatef(0, 0.5, 0)
-	
+
 	glBegin(GL_TRIANGLES)
 	glColor3f( 1, 0, 0 )
 	glVertex3f( -1, -1, 0 )
@@ -27,9 +24,22 @@ def disegna():
 	glColor3f( 0, 0, 1 )
 	glVertex3f( 1, -1, 0 )
 	glEnd()
-	
+
 	glPopMatrix()
+
+
+def disegna(zoom):
+	glClearColor(0,0,0,1);
+	glClear(GL_COLOR_BUFFER_BIT)
 	
+	glLoadIdentity()
+
+	glPushMatrix()
+	glTranslatef(0, 0, zoom)
+
+	triangolo()
+	glPopMatrix()
+
 	pygame.display.flip()
 
 
@@ -37,9 +47,12 @@ def main():
 	pygame.init()
 	display = (500, 500)
 	screen = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-	
-	gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
+	glMatrixMode(GL_PROJECTION)
+	gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+	
+	glMatrixMode(GL_MODELVIEW)
+	zoom = 0;
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -49,7 +62,17 @@ def main():
 			if event.type == pygame.KEYUP and event.unicode == 'q':
 				return
 
-		disegna()
+			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 4:
+				zoom+=1
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_PLUS:
+				zoom+=0.5
+				
+			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 5:
+				zoom-=1
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_MINUS:
+				zoom-=0.5
+
+		disegna(zoom)
 
 
 if __name__ == "__main__":
