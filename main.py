842bda3203fb2b205ebe2c1cc211762366d02276
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-12 08:58:03 +0100

    Rotazione del modello
    Aggiungiamo la rotazione del modello attorno ai propri assi x ed y, in funzione del movimento del mouse.

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ from OpenGL.GLU import *
 def triangolo():
 	glPushMatrix()
 
-	glRotatef(10, 0, 0, 1)
-	glScalef(0.7, 0.5, 1)
-	glTranslatef(0, 0, -1.0)
-	glTranslatef(0, 0.5, 0)
-
 	glBegin(GL_TRIANGLES)
 	glColor3f( 1, 0, 0 )
 	glVertex3f( -1, -1, 0 )
@@ -28,14 +23,17 @@ def triangolo():
 	glPopMatrix()
 
 
-def disegna(zoom):
+def disegna(zoom, rotx, roty):
 	glClearColor(0,0,0,1);
 	glClear(GL_COLOR_BUFFER_BIT)
-	
-	glLoadIdentity()
 
+	glLoadIdentity()
 	glPushMatrix()
+
 	glTranslatef(0, 0, zoom)
+	glTranslatef(0, 0, -5)
+	glRotatef(-rotx, 1, 0, 0)
+	glRotatef(-roty, 0, 1, 0)
 
 	triangolo()
 	glPopMatrix()
@@ -50,29 +48,47 @@ def main():
 
 	glMatrixMode(GL_PROJECTION)
 	gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-	
+
 	glMatrixMode(GL_MODELVIEW)
-	zoom = 0;
+	zoom = 0
+	old_x, old_y, rotx, roty = 0,0,0,0;
+	dragging = False
 	while True:
 		for event in pygame.event.get():
+			# Uscita dal programma
 			if event.type == pygame.QUIT:
 				return
 			if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
 				return
 			if event.type == pygame.KEYUP and event.unicode == 'q':
 				return
-
+			
+			# Zoom in
 			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 4:
 				zoom+=1
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_PLUS:
 				zoom+=0.5
-				
+
+			# Zoom out
 			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 5:
 				zoom-=1
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_MINUS:
 				zoom-=0.5
 
-		disegna(zoom)
+			# Rotazioni del modello
+			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+				dragging = True
+				old_x, old_y = event.pos
+			if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+				dragging = False
+			if event.type == pygame.MOUSEMOTION:
+				if dragging:
+					rotx = rotx - (event.pos[1] - old_y)
+					roty = roty - (event.pos[0] - old_x)
+					old_x = event.pos[0]
+					old_y = event.pos[1]
+
+		disegna(zoom, rotx, roty)
 
 
 if __name__ == "__main__":
