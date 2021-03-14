3a54a2d70591470571e141327d7de87119a43dcc
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-14 20:34:19 +0100

    Disegniamo un oggetto solido
    Carichiamo nella GPU il primo oggetto solido: un cubo!

--- main.py
+++ main.py
@@ -9,40 +9,56 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-def toFloat(array):
-	return np.array(array, dtype='float32')
+def genera_cubo():
+	vertici = np.array([[ 1, -1, -1], [ 1,	1, -1], [-1,  1, -1], [-1, -1, -1],
+						[ 1, -1,  1], [ 1,	1,	1], [-1, -1,  1], [-1,	1,	1]], dtype='float32')
 
+	colori = np.array([[0,1,0], [0,0,1], [1,0,0], [1,1,1]], dtype='float32')
 
-def triangolo():
-	vertici = [-1, -1, 0,
-				1, -1, 0,
-				0,	1, 0]
-	colori = [1, 0, 0,
-			  0, 0, 1,
-			  0, 1, 0]
+	superfici = np.array([[3,2,1,0], [6,7,2,3], [4,5,7,6],
+						  [0,1,5,4], [2,7,5,1], [6,3,0,4]]).reshape(-1)
+
+	vbo1 = vertici[superfici].reshape(-1)
+	vbo2 = np.tile(colori, (6, 1)).reshape(-1)
 
 	vertex_bufferId = glGenBuffers(1)
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_bufferId)
-	glBufferData(GL_ARRAY_BUFFER, 4*len(vertici), toFloat(vertici), GL_STATIC_DRAW)
-	glFinish()
-	glVertexPointer(3, GL_FLOAT, 0, None )
+	glBufferData(GL_ARRAY_BUFFER, 4*len(vbo1), vbo1, GL_STATIC_DRAW)
 
 	color_bufferId = glGenBuffers(1)
 	glBindBuffer(GL_ARRAY_BUFFER, color_bufferId)
-	glBufferData(GL_ARRAY_BUFFER, 4*len(colori), toFloat(colori), GL_STATIC_DRAW)
-	glFinish()
-	glColorPointer(3, GL_FLOAT, 0, None )
+	glBufferData(GL_ARRAY_BUFFER, 4*len(vbo2), vbo2, GL_STATIC_DRAW)
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0)
 
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
+	return {'v':vertex_bufferId, 'c':color_bufferId, 'type':GL_QUADS, 'len':len(superfici)}
+
+
+def disegna_mesh(mesh, colore=None):
+	glEnableClientState(GL_VERTEX_ARRAY)
+	if colore is None:
+		glEnableClientState(GL_COLOR_ARRAY)
+
+	glBindBuffer(GL_ARRAY_BUFFER, mesh['v'])
+	glVertexPointer(3, GL_FLOAT, 0, None )
+	if colore is None:
+		glBindBuffer(GL_ARRAY_BUFFER, mesh['c'])
+		glColorPointer(3, GL_FLOAT, 0, None )
+	glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+	if colore is not None:
+		glColor3fv(colore)
+	glDrawArrays(mesh['type'], 0, mesh['len'])
+
+	glDisableClientState(GL_VERTEX_ARRAY)
+	if colore is None:
+		glDisableClientState(GL_COLOR_ARRAY)
 
 
 animation_angle = 0
-def disegna(zoom, rotx, roty, animate):
-	glClearColor(0,0,0,1);
-	glClear(GL_COLOR_BUFFER_BIT)
+def disegna(zoom, rotx, roty, animate, wireframe, mesh):
+	glClearColor(0,0,0,1)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
 	glLoadIdentity()
 	glPushMatrix()
@@ -57,15 +73,11 @@ def disegna(zoom, rotx, roty, animate):
 	if animate:
 		animation_angle += 1
 
-	glPushMatrix()
-	glTranslatef(-1, 0, 0)
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glPopMatrix()
-
-	glPushMatrix()
-	glTranslatef(1, 0, 0)
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glPopMatrix()
+	disegna_mesh(mesh)
+	glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )
+	disegna_mesh(mesh, [1,1,1])
+	if not wireframe:
+		glPolygonMode( GL_FRONT_AND_BACK, GL_FILL )
 
 	glPopMatrix()
 
@@ -82,16 +94,19 @@ def main():
 
 	glEnable(GL_CULL_FACE)
 	glCullFace(GL_BACK)
+	glEnable(GL_DEPTH_TEST)
 
-	triangolo()
+	mesh = genera_cubo()
 
 	glMatrixMode(GL_MODELVIEW)
 	zoom = 0
-	old_x, old_y, rotx, roty = 0,0,0,0;
+	old_x, old_y, rotx, roty = 0,0,0,0
 	dragging = False
 	animate = False
 	clock = pygame.time.Clock()
 	wireframe = False
+	culling = True
+	dept_test = True
 	while True:
 		for event in pygame.event.get():
 			# Uscita dal programma
@@ -145,12 +160,28 @@ def main():
 			# Attivazione wireframe
 			if event.type == pygame.KEYUP and event.unicode == 'w':
 				if wireframe:
-					glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+					glPolygonMode( GL_FRONT_AND_BACK, GL_FILL )
 				else:
-					glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+					glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )
 				wireframe = not wireframe
 
-		disegna(zoom, rotx, roty, animate)
+			# Attivazione culling
+			if event.type == pygame.KEYUP and event.unicode == 'c':
+				if culling:
+					glDisable(GL_CULL_FACE)
+				else:
+					glEnable(GL_CULL_FACE)
+				culling = not culling
+
+			# Attivazione dept test
+			if event.type == pygame.KEYUP and event.unicode == 'd':
+				if dept_test:
+					glDisable(GL_DEPTH_TEST)
+				else:
+					glEnable(GL_DEPTH_TEST)
+				dept_test = not dept_test
+
+		disegna(zoom, rotx, roty, animate, wireframe, mesh)
 		clock.tick(30)
 
 
