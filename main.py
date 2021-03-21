654f6b31a0083e1f5ed5cd204dbd503b5747d10e
Daniele Prevedello <dprevedello86@gmail.com>
2021-03-21 21:19:09 +0100

    Applichiamo una texture
    Utilizzando gli shader, visualizziamo un modello dotato di texture.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import traceback
 import numpy as np
 import glm
+import cv2
 
 import pygame
 from pygame.locals import *
@@ -17,10 +18,10 @@ vertex_shader = """
 
 // Dati in input, variabili per ciascun vertice
 layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec3 vertexColor;
+layout(location = 1) in vec2 vertexUVcoords;
 
 // Dati in output, interpolati per ciascun frammento
-out vec3 fragmentColor;
+out vec2 UV;
 
 // Valori passati allo shader che rimangono costanti
 uniform mat4 MVP;
@@ -29,9 +30,8 @@ void main(){
 	// Calcolo della posizione del vertice in clip space: MVP * position
 	gl_Position =  MVP * vec4(vertexPosition_modelspace, 1);
 
-	// Il colore di ciascun vertice viene interpolato per
-	// ottenere il colore di ciascun frammento
-	fragmentColor = vertexColor;
+	// Coordinate UV del vertice.
+	UV = vertexUVcoords;
 }
 """
 
@@ -39,13 +39,16 @@ fragment_shader = """
 #version 330 core
 
 // Valori interpolati forniti dal vertex shader
-in vec3 fragmentColor;
+in vec2 UV;
 
 // Dati in output
 out vec3 color;
 
+// Valori costanti per tutta l'esecuzione
+uniform sampler2D textureSampler;
+
 void main(){
-	color = fragmentColor;
+	color = texture2D( textureSampler, UV ).rgb;
 }
 """
 
@@ -59,7 +62,7 @@ def compile_shaders():
 	glShaderSource(vs, vertex_shader)
 	glCompileShader(vs)
 	if GL_TRUE != glGetShaderiv(vs, GL_COMPILE_STATUS):
-		print(glGetShaderInfoLog(vs))
+		raise Exception(glGetShaderInfoLog(vs))
 	glAttachShader(program, vs)
 
 	# fragment shader
@@ -67,45 +70,61 @@ def compile_shaders():
 	glShaderSource(fs, fragment_shader)
 	glCompileShader(fs)
 	if GL_TRUE != glGetShaderiv(vs, GL_COMPILE_STATUS) :
-		print(glGetShaderInfoLog(vs))
+		raise Exception(glGetShaderInfoLog(vs))
 	glAttachShader(program, fs)
 
 	glLinkProgram(program)
 	if GL_TRUE != glGetProgramiv(program, GL_LINK_STATUS):
-		print(glGetProgramInfoLog(program))
+		raise Exception(glGetProgramInfoLog(program))
 
 	MVP_ID = glGetUniformLocation(program, "MVP")
+	TS_ID = glGetUniformLocation(program, "textureSampler")
 
-	return {'shader':program, 'MVP':MVP_ID}
+	return {'shader':program, 'MVP':MVP_ID, 'sampler':TS_ID}
 
 
-def carica_mesh(path):
+def carica_mesh(path, texture_path):
 	shader = compile_shaders()
 
 	mesh = Wavefront(path)
+	print(mesh.mesh_list[0].materials[0].vertex_format)
+
 	data = np.array([])
 	for m_item in mesh.mesh_list:
 		for mat in m_item.materials:
 			data = np.append(data, mat.vertices)
-	dim = len(data)//6
-	data = data.reshape(dim, 2, 3).astype(np.float32)
-	vertici = data[:,1].reshape(-1)
-	colori = data[:,0].reshape(-1)
+	dim = len(data)//8
+	data = data.reshape(dim, 8).astype(np.float32)
+	vertici = data[:,5:8].reshape(-1)
+	texel = data[:,0:2].reshape(-1)
 
 	vertex_bufferId = glGenBuffers(1)
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_bufferId)
 	glBufferData(GL_ARRAY_BUFFER, 4*len(vertici), vertici, GL_STATIC_DRAW)
 
-	color_bufferId = glGenBuffers(1)
-	glBindBuffer(GL_ARRAY_BUFFER, color_bufferId)
-	glBufferData(GL_ARRAY_BUFFER, 4*len(colori), colori, GL_STATIC_DRAW)
+	texel_bufferId = glGenBuffers(1)
+	glBindBuffer(GL_ARRAY_BUFFER, texel_bufferId)
+	glBufferData(GL_ARRAY_BUFFER, 4*len(texel), texel, GL_STATIC_DRAW)
+
+	texture = glGenTextures(1)
+	glBindTexture(GL_TEXTURE_2D, texture)
+	img = cv2.flip(cv2.imread(texture_path), 0)
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.shape[1], img.shape[0], 0, GL_BGR, GL_UNSIGNED_BYTE, img.data)
+	glGenerateMipmap(GL_TEXTURE_2D)
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0)
+	glBindTexture(GL_TEXTURE_2D, 0)
 
 	identity = glm.mat4(1.0)
 
-	return shader | {'v':vertex_bufferId, 'c':color_bufferId, 'type':GL_TRIANGLES,
-					 'n_triangoli':dim, 'model_m':identity}
+	return shader | {'v':vertex_bufferId, 't':texel_bufferId, 'texture':texture,
+					 'type':GL_TRIANGLES, 'n_triangoli':dim, 'model_m':identity}
+
+
+def libera_risorse_mesh(mesh):
+	glDeleteBuffers(1, mesh['v'])
+	glDeleteBuffers(1, mesh['t'])
+	glDeleteTexture(1, mesh['texture'])
 
 
 animation_angle = 0
@@ -132,16 +151,24 @@ def disegna(zoom, rotx, roty, animate, view_m, proj_m, mesh):
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
 
 	glEnableVertexAttribArray(1)
-	glBindBuffer(GL_ARRAY_BUFFER, mesh['c'])
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, None)
+	glBindBuffer(GL_ARRAY_BUFFER, mesh['t'])
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, None)
 
-	glDrawArrays(GL_TRIANGLES, 0, mesh['n_triangoli']*3)
+	glUniform1i(mesh['sampler'], 0)
+	glBindTexture(GL_TEXTURE_2D, mesh['texture'])
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+	glDrawArrays(mesh['type'], 0, mesh['n_triangoli']*3)
 
 	glDisableVertexAttribArray(0)
 	glDisableVertexAttribArray(1)
 
 	glUseProgram(0)
 	glBindBuffer(GL_ARRAY_BUFFER, 0)
+	glBindTexture(GL_TEXTURE_2D, 0)
 
 	pygame.display.flip()
 
@@ -160,7 +187,7 @@ def main():
 	glCullFace(GL_BACK)
 	glEnable(GL_DEPTH_TEST)
 
-	mesh = carica_mesh('./mesh/box-C3F_V3F.obj')
+	mesh = carica_mesh('./mesh/earth.obj', './mesh/earthmap.2k.jpg')
 
 	zoom = 0
 	old_x, old_y, rotx, roty = 0,0,0,0
@@ -243,6 +270,7 @@ def main():
 
 		disegna(zoom, rotx, roty, animate, view_m, proj_m, mesh)
 		clock.tick(30)
+	libera_risorse_mesh(mesh)
 
 
 if __name__ == "__main__":
